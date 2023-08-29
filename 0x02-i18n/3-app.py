0e071cc865019ccb5eec22d605f3b33eac670872
sezfabian <cheruiyotfabian@gmail.com>
2023-08-30 02:53:53 +0300

    babel

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -37,7 +37,8 @@ def index():
     """
     home_title = _("Welcome to Holberton")
     home_header = _("Hello world")
-    return render_template("3-index.html", home_title=home_title, home_header=home_header)
+    return render_template("3-index.html",
+                           home_title=home_title, home_header=home_header)
 
 
 if __name__ == "__main__":
