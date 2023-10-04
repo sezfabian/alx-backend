c7312f7c3ef1914ffb9bfb444da4835ef044c5a4
sezfabian <cheruiyotfabian@gmail.com>
2023-10-04 10:22:37 +0300

    job creation

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const kue = require('kue');
+
+const queue = kue.createQueue();
+
+const blackList = ['4153518780','4153518781'];
+
+const sendNotification = (phoneNumber, message, job, done) => {
+    let total = 2, pending = 2;
+    let sendInterval = setInterval(() => {
+      if (total - pending <= total / 2) {
+        job.progress(total - pending, total);
+      }
+      if (blackList.includes(phoneNumber)) {
+        done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+        clearInterval(sendInterval);
+        return;
+      }
+      if (total === pending) {
+        console.log(
+          `Sending notification to ${phoneNumber},`,
+          `with message: ${message}`,
+        );
+      }
+      --pending || done();
+      pending || clearInterval(sendInterval);
+    }, 1000);
+  };
+  
+  queue.process('push_notification_code_2', 2, (job, done) => {
+    sendNotification(job.data.phoneNumber, job.data.message, job, done);
+  });
